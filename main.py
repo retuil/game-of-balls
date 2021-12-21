cb3c9a167b1991977711beb14daff55a00d79fa8
AndreyRomanov <aromanovekb@gmail.com>
2021-12-21 14:14:48 +0500

    main

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import pygame
 from board import Board
+from ball import Ball
 
 
 if __name__ == '__main__':
     pygame.init()
-    size = width, height = 800, 800
+    size = width, height = 1000, 1000
     screen = pygame.display.set_mode(size)
-    board = Board(7, 13)
+    board = Board(8, 11)
     board.set_view(100, 100, 50)
     running = True
+    clock = pygame.time.Clock()
+    draw = False
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                board.get_click(event.pos)
+                draw = True
+                x0, y0 = event.pos
+                board.balls.append(Ball(x0, y0, board, 5))
         screen.fill((0, 0, 0))
-        board.render(screen)
+        board.render(screen, clock, draw)
         pygame.display.flip()
     pygame.quit()
