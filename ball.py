e9861729d2b95f13ae923fc3a32d12409992c59b
AndreyRomanov <aromanovekb@gmail.com>
2021-12-23 19:10:33 +0500

    ball v2 шар это спрайт, отражения не через спрайты

--- ball.py
+++ ball.py
@@ -1,3 +1,5 @@
+import os
+import sys
 from math import sqrt
 
 import pygame
@@ -5,9 +7,8 @@ import pygame
 
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
-    # если файл не существует, то выходим
     if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
+        print(f"'{fullname}' error")
         sys.exit()
     image = pygame.image.load(fullname)
     return image
@@ -18,11 +19,22 @@ class Ball(pygame.sprite.Sprite):
 
     def __init__(self, x0, y0, board, r=5, *group):
         super().__init__(*group)
-        self.x = board.left + board.width * board.cell_size // 2
-        self.y = board.top + board.height * board.cell_size - r - 1
-        x0, y0 = x0 - (board.left + board.width * board.cell_size // 2), board.top + board.height * board.cell_size - y0
+        self.image = Ball.image
+        self.rect = self.image.get_rect()
+        self.rect.x = board.left + board.width * board.cell_size // 2 - r
+        self.rect.y = board.top + board.height * board.cell_size - 2 * r - 2
+        self.x1 = self.rect.x
+        self.y1 = self.rect.y
+        x0 = x0 - (board.left + board.width * board.cell_size // 2)
+        y0 = board.top + board.height * board.cell_size - y0 - 2
         x0, y0 = x0 / sqrt(x0 ** 2 + y0 ** 2), y0 / sqrt(x0 ** 2 + y0 ** 2)
         self.x0 = x0
         self.y0 = -y0
         self.v = 800
         self.r = r
+        board.all_sprites.add(self)
+
+    def update(self, c):
+        self.x1 += self.x0 * self.v * c / 1000
+        self.y1 += self.y0 * self.v * c / 1000
+        self.rect.x, self.rect.y = round(self.x1), round(self.y1)
