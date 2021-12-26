2f9ade434714e54d1fcee572877b72152e080ee9
AndreyRomanov <aromanovekb@gmail.com>
2021-12-26 21:35:38 +0500

    v6.2 вернул клеточную сетку

--- board.py
+++ board.py
@@ -80,8 +80,17 @@ class Board:
             pygame.draw.line(screen, (30, 30, 30),
                              (self.left + self.width * self.cell_size // 2,
                               self.top + self.height * self.cell_size - 5 - 1), (int(x0), int(y0)), width=1)
-
         self.all_sprites.draw(screen)
+        for i in range(self.height):
+            for j in range(self.width):
+                x = self.left + self.cell_size * j
+                y = self.top + self.cell_size * i
+                if (i >= self.height - 1) or (i <= 1):
+                    pass
+                elif self.debug:
+                    pygame.draw.rect(screen, pygame.Color('red'), (x, y, self.cell_size, self.cell_size), width=1)
+                else:
+                    pygame.draw.rect(screen, (2, 2, 2), (x, y, self.cell_size, self.cell_size), width=1)
         self.borders.draw(screen)
 
     def on_click(self):
