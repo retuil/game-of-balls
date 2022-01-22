4fe8c828f8085426708b4f5acabd941cf6e9f795
retuil <surganov.denis@gmail.com>
2022-01-22 18:06:07 +0500

    добавленно создание кнопки

--- StartMenu.py
+++ StartMenu.py
@@ -6,10 +6,18 @@ from MyGroup import MyGroup
 
 class StartMenu:
     def __init__(self):
-        screen = pygame.display.set_mode((800, 800)).fill((0, 0, 0))
+        screen = pygame.display.set_mode((800, 800))
+        screen.fill((255, 255, 255))
         running = True
+        group = MyGroup()
+        button = Button(0, group, size=[50, 100], pos=[10, 10], color='yellow', screen=screen)
+        group.draw(screen)
+        pygame.display.flip()
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-        MyGroup.draw(screen)
+        group.draw(screen)
+        pygame.display.flip()
+
+a = StartMenu()
