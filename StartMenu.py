a7046c302fc7858135d9e30b5384e3fbaecea839
retuil <surganov.denis@gmail.com>
2022-01-25 17:58:09 +0500

    изменен принцип создания Button и MyGroup

--- StartMenu.py
+++ StartMenu.py
@@ -9,8 +9,9 @@ class StartMenu:
         screen = pygame.display.set_mode((800, 800))
         screen.fill((255, 255, 255))
         running = True
-        group = MyGroup()
-        button = Button(0, group, size=[50, 100], pos=[10, 10], color='yellow', screen=screen)
+        group = MyGroup([10, 10], [10, 10], [50, 100])
+        button = Button(0, group, color='yellow')
+        button = Button(1, group, color='yellow')
         group.draw(screen)
         pygame.display.flip()
         while running:
