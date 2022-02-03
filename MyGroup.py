25a987574944330e253d73c70105fd7bb831ac02
Admin <surganov.denis@gmail.com>
2022-02-03 18:38:01 +0500

    Добавлено меню выбора режима (продолжить, новая игра, таблица лидеров)

--- MyGroup.py
+++ MyGroup.py
@@ -5,4 +5,6 @@ class MyGroup(pygame.sprite.Group):
     def check_any_click(self, mouse_pos):
         for sprite in self.sprites():
             if sprite.check_click_button(mouse_pos):
+                return 1
                 break
+        return 0
